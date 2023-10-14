58f6b983f617d1094bd14e768e730b5824cbdba3
WHITNEYSHILLINGI7 <139699359+WHITNEYSHILLINGI7@users.noreply.github.com>
2023-10-15 00:43:51 +0300

    Add files via upload

---
+++ honour.js
@@ -0,0 +1,23 @@
+// Simple javascript code
+//The console.log() function is commonly used for debugging and logging information during development.
+//The console.log() statements are used to print the values of these variables to the console.
+console.log("hello, world!")
+
+// Log the values of the variables to the console
+console.log("Number Variable:", 17);
+console.log("String Variable:", Hennesse);
+console.log("Boolean Variable:", true);
+
+// Declare variables with different data types
+//  Variables declared with var are function-scoped, This means they are only available within the function where they are declared. 
+var numberVar = 42;         // Number
+var stringVar = "Hello";    // String
+var booleanVar = true;      // Boolean
+
+// Variables declared with let are block-scoped, This means they are only accessible within the block, statement, or expression where they are defined. 
+// Variable declaration and initialization
+let numberVar = 42; // Number
+let stringVar = "Hello, World!"; // String
+let booleanVar = true; // Boolean
+
+
